82f8626e04709d237962b4a7c250ad22d4648fad
Pashkok-orig <5n9m794mf6@privaterelay.appleid.com>
2026-09-14 22:40:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const C='dnevnik-v1';
+const C='dnevnik-v3';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.add('./')));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));return res;}).catch(()=>caches.match('./'))));});
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.method!=='GET')return;
+  if(req.mode==='navigate'){
+    e.respondWith(fetch(req).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put('./',cp));return res;}).catch(()=>caches.match('./')));
+    return;
+  }
+  e.respondWith(caches.match(req,{ignoreSearch:true}).then(r=>r||fetch(req).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(req,cp));return res;})));
+});
